e0580718875f5f650c7f486c0b2908ee031d6fd1
Pitsyk Aleksandr <pitsyk.a@gmail.com>
2025-01-14 16:24:10 +0200

    День - 14.01.2025
    Закончено мобильное меню.
    Осталось - адаптивная графика и фото

--- js/modal.js
+++ js/modal.js
@@ -9,6 +9,10 @@
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
+    // При открытом модальном окне устанавливаются дополнительные классы
+    // На тег body
+    document.body.classList.toggle("modal-open");
+    // На константу modal - точнее на тот элемент, у которого в теге есть атрибут data-modal
     refs.modal.classList.toggle("is-hidden");
   }
 })();
